4e35ad0c30af596a1a248da5a498e003b62a7a7d
sakhiasyam <85838214+sakhiasyam18@users.noreply.github.com>
2026-04-19 10:00:44 +0000

    Latihan: React Element

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-const element = <h1>Hello Word</h1>;
+const heading = React.createElement('h1', null, 'Biodata Asyam SM hehe');
+
+const listItem1 = React.createElement('li', null, 'Nama : Uwuwuwuu Dicoding nih');
+const listItem2 = React.createElement('li', null, 'Bidang : Informatika garis keras');
+const listItem3 = React.createElement('li', null, 'Tagline : WEB 3 BTC Bossss');
+
+const unorderedList = React.createElement('ul', null, [listItem1, listItem2, listItem3]);
+
+const container = React.createElement('div', null, [heading, unorderedList]);
 
 const root = createRoot(document.getElementById('root'));
-root.render(element);
+root.render(container);
