d367001f1181369776ffa8d3c5ddc65cec72bb0d
sakhiasyam <85838214+sakhiasyam18@users.noreply.github.com>
2026-04-20 07:57:52 +0000

    Latihan Menggunakan JSX

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
+// [1/]Latihan Menggunakan JSX 19 april 2026
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import DicodingLogo from './dicoding-logo.png';
 
-const heading = React.createElement('h1', null, 'Biodata Asyam SM hehe');
-
-const listItem1 = React.createElement('li', null, 'Nama : Uwuwuwuu Dicoding nih');
-const listItem2 = React.createElement('li', null, 'Bidang : Informatika garis keras');
-const listItem3 = React.createElement('li', null, 'Tagline : WEB 3 BTC Bossss');
-
-const unorderedList = React.createElement('ul', null, [listItem1, listItem2, listItem3]);
-
-const container = React.createElement('div', null, [heading, unorderedList]);
+const element = (
+  <div>
+    <h1>Biodata Asyam Tapi versi React</h1>
+    <ul>
+      <li>Nama: Dicoding Indonesia</li>
+      <li>Bidang: Education</li>
+      <li>Tagline: Decode Ideas, Discover Potential.</li>
+      <img src="dicoding-logo.png" alt="logo dicoding" />
+      <img src={DicodingLogo} alt="Dicoding logonya" />
+    </ul>
+  </div>
+);
 
 const root = createRoot(document.getElementById('root'));
-root.render(container);
+root.render(element);
